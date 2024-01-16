a46d6f8b25d50b31f1400cb56cb0d35b7a91bf0a
Islamsaeedfahmy <121707916+Islamsaeedfahmy@users.noreply.github.com>
2024-01-17 00:21:46 +0200

    Add files via upload
    first--commit-isdone

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Todo from "./components/Todo"
+import TodoItem from "./components/TodoItem"
+
+ const App = () => {
+
+
+
+  return (
+    <div>
+      <Todo />
+      <TodoItem />
+   
+    </div>
+  )
+}
+export default App
+
